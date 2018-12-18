330b086e12215fd361c13779d5b735e0abf72d76
Romy Maghsoudi <romy_mag@outlook.com>
2018-12-18 23:02:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ function divide(a, b){
   return a / b
 }
 
-function add(a, b){
-  return a + b
+function inc(n){
+  return n++
+}
+
+function dec(n){
+  return n--
+}
+
+function inc(n){
+  return n++
 }
